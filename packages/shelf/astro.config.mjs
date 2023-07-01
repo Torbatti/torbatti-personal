535407c0bcf3fd6b1aa75f899b1840793bb4ea4e
torbatti <torbattii@gmail.com>
2023-07-01 19:33:34 +0330

    updated pages

--- packages/shelf/astro.config.mjs
+++ packages/shelf/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.torbatti.ir',
-  base: '/shelf',
+  base: '/shelf/',
   outDir: '../../dist/shelf/',
 });
